53b5cd924175e6ec844cb22409f65c1ce553d61f
ihorpidhornyi <ihorp@rabota.ua>
2022-07-23 22:37:13 +0300

    feat(map): include header height

--- src/components/Map/Map.styled.ts
+++ src/components/Map/Map.styled.ts
@@ -3,7 +3,8 @@ import { css } from '@emotion/react'
 
 export const Container = styled.div(
   css({
+      marginTop: '56px',
     width: '100vw',
-    height: '100vh',
+    height: 'calc(100vh - 56px)',
   })
 )
